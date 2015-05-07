83abf3c2b6343e6c29a5f545e8509f2319093870
Jacob Evelyn <jevelyn@panoramaed.com>
2015-05-06 20:04:36 -0400

    Initial Commit

---
+++ Compute for Humanity Menu Bar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Compute for Humanity Menu Bar
+//
+//  Created by Jacob Evelyn on 5/6/15.
+//  Copyright (c) 2015 Jacob Evelyn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
